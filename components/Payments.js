61ec1528abbb5cfbbefb9cd468f68ba2ce8f1f7f
Elizabeth Renderos <elirenderos@gmail.com>
2023-05-21 09:09:29 +0900

    fixed dependency bug

--- components/Payments.js
+++ components/Payments.js
@@ -21,7 +21,7 @@ const ButtonWrapper = ({ currency, showSpinner, amount }) => {
                 currency: currency,
             },
         });
-    }, [currency, showSpinner, dispatch, options]);
+    }, [currency, showSpinner]);
 
     return (
     <>
