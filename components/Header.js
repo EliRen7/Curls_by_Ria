82b7081571110f3973f6f3d1ddb3c1a2ca7d4ba3
Elizabeth Renderos <elirenderos@gmail.com>
2023-05-21 14:28:58 +0900

    worked on mobile responsive for header

--- components/Header.js
+++ components/Header.js
@@ -6,11 +6,11 @@ import logo from "../public/assets/curlsbyrialogo2.png"
 
 function Header(){
     return( 
-<div className="w-max min-h-screen md:w-full pb-60 md:pb-0 md:hero md:min-h-screen bg-rose-50" id="home">
-<div className="hero-content flex-col lg:flex-row-reverse ">
+<div className="w-[80rem] min-h-screen md:w-full pb-96 md:pb-0 md:hero md:min-h-screen bg-rose-50" id="home">
+<div className="hero-content  flex-col lg:flex-row-reverse ">
   <Image
       src={mainPic} 
-      className="w-auto mt-2/4 md:visible md:max-w-md md:mt-20 md:h-full md:ml-0"
+      className="w-auto mt-[12rem] md:visible md:max-w-md md:mt-20 md:h-full md:ml-0"
       alt='img'/>
   <div>
   <Image 
@@ -18,10 +18,10 @@ function Header(){
       className="mr-20 w-full mt-10 md:w-9/12 md:mr-20 md:mt-5 md:ml-5" 
       alt='img' 
       data-aos="fade-down-right"/>
-    <p className="invisible md:visible md:py-4 md:p-28 md:text-2xl  text-rose-300 font-about" data-aos="fade-left"> 
+    <p className="text-5xl p-7 md:py-4 md:p-28 md:text-2xl  text-rose-300 font-about" data-aos="fade-left"> 
     ✨South Korea&apos;s first official curl specialist!✨ 
      </p> 
-     <p className="invisible md:visible md:py-0 md:p-40 md:text-2xl font-koreanHeader" data-aos="fade-left">
+     <p className="text-5xl p-7 md:py-0 ml-10 md:ml-0 md:p-40 md:text-2xl font-koreanHeader" data-aos="fade-left">
      대한민국에 최초의 곱슬머리 전문 헤어살롱 
      </p>
      <div className='z-50 fixed ml-[60rem] h-28 w-28 rounded-full md:w-10 md:h-10 md:ml-[81rem] bg-gradient-to-r from-rose-100 to-rose-300 md:rounded-3xl opacity-60 group-hover:opacity-100 transition duration-1000 group-hover:duration-200" data-aos="fade-left "' data-aos="fade-up">
@@ -40,3 +40,6 @@ export default Header;
 
 
 
+
+
+// invisible md:visible
