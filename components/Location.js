da5055a507a9f16debe9e6759e507bb54c20c43d
Elizabeth Renderos <84453538+EliRen7@users.noreply.github.com>
2023-06-28 20:20:54 +0900

    Update Location.js

--- components/Location.js
+++ components/Location.js
@@ -19,9 +19,9 @@ export default function Location(){
  <div id="englishlocation" className="carousel-item w-full">
      <p className="indent-9 pl-20 md:p-20 text-7xl md:text-lg text-justify mt-60 md:mt-[0rem]" > 
      It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
-      The point of using Lorem Ipsum is that it has a  normal distribution of letters, as opposed to using  making it look like readable
-       English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have 
-     evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+      The point of using Lorem Ipsum is that it has a  normal distribution of letters as opposed to using  making it look like readable
+       English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their  model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have 
+     evolved over the years, sometimes by accident, sometimes on purpose injected humour and the like.
  </p>
  </div>
  
