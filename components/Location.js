37db9ef948ee0053b10a041953ee60cf3a9f436f
Elizabeth Renderos <84453538+EliRen7@users.noreply.github.com>
2023-06-28 21:28:49 +0900

    Update Location.js

--- components/Location.js
+++ components/Location.js
@@ -26,7 +26,7 @@ export default function Location(){
  </div>
  
        <div id="koreanlocation" className="carousel-item w-full">
-         <p className="indent-9 pl-20 pr-20 md:p-20 text-7xl md:text-lg text-justify mt-60 md:mt-[0rem]" > 
+         <p className="indent-9 pl-20 pr-20 md:pl-20 text-7xl md:text-lg text-justify mt-60 md:mt-[0rem]" > 
          안녕하세요 여러분! 저는 리아라고 하고요, Curls by Ria의 대표입니다 저는  때부터 미용에 대해 열정과 관심이 있었습니다. 그래서 제가 사랑하는 것과 잘하는 것과 다른 사람들이 필요하는 것을 하는 상황이 나타났을 때 운명처럼 보였습니다. 2021년부터 제가 최고의 곱슬머리 전문가가 되기 위해 많은 투자를 했습니다. 그동안  Curly Hair Artistry (반곱슬~곱슬머리 기술 교육) 마스터 프로그램과 Cut it Kinky(강한곱슬머리 기술 교육)를 통해 곱슬머리 전문적인 교육을 받았습니다. 그리고 고객들에게 더 많은 서비스를 제공할 수 있게  이용사 국가기술자격증을 취득했습니다.
          이제, 그 지루한 내용은 그만~! 저는 핑크색에 아주아주 사랑하는 포켓몬 띠부실를 모으는 미국 교포일 뿐이에요 ㅋㅋㅋ 아! 그리고 저는 항상 강아지 두 마리(감자와 슌돌)와 함께 일하</p>
    </div>
