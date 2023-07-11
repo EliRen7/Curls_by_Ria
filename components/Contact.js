b58a13402fa0281fb97f26f0c84fe326a0b49d18
Elizabeth Renderos <84453538+EliRen7@users.noreply.github.com>
2023-07-11 20:36:05 +0900

    Update Contact.js

--- components/Contact.js
+++ components/Contact.js
@@ -9,15 +9,15 @@ function Contact () {
       <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 text-5xl md:text-xl sm:text-xl">Got a technical issue? Want to send feedback about a service? Need more details about a treatment option? Let me know.</p>
       <form action="https://formcarry.com/s/5pQxq6cPy5" method="POST" accept-charset="UTF-8" className="space-y-8">
           <div>
-              <label for="email" className="block mb-2 text-5xl md:text-sm font-medium text-gray-900 dark:text-gray-400 ">Your email</label>
+              <label for="email" className="block mb-2 text-5xl md:text-sm font-medium text-gray-300 dark:text-gray-400 ">Your email</label>
               <input name="email" type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-4xl md:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:bg-white dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@curlsbyria.com" required />
           </div>
           <div>
-              <label for="subject" className="block mb-2 text-5xl md:text-sm font-medium text-gray-900 dark:text-gray-400 ">Subject</label>
+              <label for="subject" className="block mb-2 text-5xl md:text-sm font-medium text-gray-300 dark:text-gray-400 ">Subject</label>
               <input name="subject"type="text" id="subject" className="block p-3 w-full text-4xl md:text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:bg-white dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Let me know how I can help you" required />
           </div>
           <div className="sm:col-span-2">
-              <label for="message" className="block mb-2 text-5xl md:text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
+              <label for="message" className="block mb-2 text-5xl md:text-sm font-medium text-gray-300 dark:text-gray-400">Your message</label>
               <textarea id="message" name='message' rows="6" className="block p-2.5 w-full text-4xl md:text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:bg-white dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
           </div>
           <button type="submit" className=" py-8 md:py-3 px-5 text-5xl md:text-sm font-medium text-center text-white rounded-lg bg-rose-300 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Submit</button>
