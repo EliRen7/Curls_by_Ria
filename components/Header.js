a2bf15f064619efd28763a81c26957ea107cd82e
Elizabeth Renderos <84453538+EliRen7@users.noreply.github.com>
2023-07-23 17:39:07 +0900

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -13,10 +13,10 @@ function Header(){
       className="mr-26 w-[92rem] mt-[60rem] md:w-9/12 md:mr-20 md:mt-5 md:ml-5" 
       alt='img' 
       data-aos="fade-down-right"/>
-    <p className="text-5xl ml-24 md:ml-0 p-10 md:py-4 md:p-28 md:text-2xl  text-black font-about" data-aos="fade-left"> 
+    <p className="text-5xl ml-32 md:ml-0 p-10 md:py-4 md:p-28 md:text-2xl text-black font-about" data-aos="fade-left"> 
     ✨South Korea&apos;s first official curl specialist!✨ 
      </p> 
-     <p className="text-5xl text-korean-description p-10 md:py-0 ml-52 md:ml-0 md:p-40 md:text-2xl font-koreanHeader" data-aos="fade-left">
+     <p className="text-5xl text-korean-description p-10 md:py-0 ml-56 md:ml-0 md:p-40 md:text-2xl font-koreanHeader" data-aos="fade-left">
      대한민국에 최초의 곱슬머리 전문 헤어살롱 
      </p>
      <div className='z-50 invisible fixed ml-[60rem] h-28 w-28 rounded-full mt-[2rem] md:mt-[7rem] md:w-10 md:h-10 md:ml-[81rem] bg-gradient-to-r from-rose-100 to-rose-300 md:rounded-3xl opacity-60 group-hover:opacity-100 transition duration-1000 group-hover:duration-200" data-aos="fade-left "' data-aos="fade-up">
