dd1d7648495619cd554300af7ba18b3b3f6a3f59
Elizabeth Renderos <84453538+EliRen7@users.noreply.github.com>
2023-07-24 21:44:28 +0900

    Update Contact.js

--- components/Contact.js
+++ components/Contact.js
@@ -3,9 +3,9 @@ import {AiOutlineArrowUp} from 'react-icons/ai'
 
 function Contact () {
     return(
-     <section className="bg-tan w-[80rem] md:w-full dark:bg-tan pt-40 pb-60" id="contact">
+     <section className="bg-white w-[84rem] md:w-full dark:bg-tan pt-40 pb-60" id="contact">
   <div className="py-8 lg:py-16 px-4 mx-auto min-h-screen-xl max-w-screen-lg md:max-w-screen-md rounded-3xl shadow-xl bg-zinc-100 ">
-      <h2 className="mb-4 text-9xl md:text-5xl tracking-tight font-extrabold text-center text-rose-300 font-headers">Contact Me!</h2>
+      <h2 className="mb-4 text-9xl md:text-5xl tracking-tight font-extrabold text-center text-korean-description font-headers">Contact Me!</h2>
       <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 text-5xl md:text-xl sm:text-xl">Got a technical issue? Want to send feedback about a service? Need more details about a treatment option? Let me know.</p>
       <form action="https://formcarry.com/s/5pQxq6cPy5" method="POST" accept-charset="UTF-8" className="space-y-8">
           <div>
@@ -20,7 +20,7 @@ function Contact () {
               <label for="message" className="block mb-2 text-5xl md:text-sm font-medium text-gray-300 dark:text-gray-400">Your message</label>
               <textarea id="message" name='message' rows="6" className="block p-2.5 w-full text-4xl md:text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:bg-white dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
           </div>
-          <button type="submit" className=" py-8 md:py-3 px-5 text-5xl md:text-sm font-medium text-center text-white rounded-lg bg-rose-300 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Submit</button>
+          <button type="submit" className=" py-8 md:py-3 px-5 text-5xl md:text-sm font-medium text-center text-white rounded-lg bg-korean-description sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Submit</button>
       </form> 
   </div>
     
