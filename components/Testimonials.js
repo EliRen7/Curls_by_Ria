d1a160bfa0702d3a6b8982982d4afa0a9283779f
Elizabeth Renderos <84453538+EliRen7@users.noreply.github.com>
2023-07-24 21:29:31 +0900

    Update Testimonials.js

--- components/Testimonials.js
+++ components/Testimonials.js
@@ -26,10 +26,10 @@ import tightCurls6 from '../public/assets/tight-curls 6.jpg'
 
 function Testimonials(){
     return(
-      <div className='bg-tan pb-20 w-[80rem] md:w-full' id='testimonials'>
+      <div className='bg-white pb-20 w-[84rem] md:w-full' id='testimonials'>
       <div className="mb-8 pt-40 text-center ">
-      <h1 className="font-headers text-9xl md:text-5xl text-rose-300" data-aos="fade-up">Testimonials <br></br> & Gallery</h1>
-      <p className=" text-5xl md:text-lg text-rose-300" data-aos="fade-up">Read more reviews on Instagram!</p>
+      <h1 className="font-headers text-9xl md:text-5xl text-korean-description" data-aos="fade-up">Testimonials <br></br> & Gallery</h1>
+      <p className=" text-5xl md:text-lg text-korean-description" data-aos="fade-up">Read more reviews on Instagram!</p>
       <a href='https://www.instagram.com/curls.byria/?hl=en' target="_blank"> <AiFillInstagram className='w-32 h-32 ml-[35rem] md:w-10 md:h-10 md:ml-[46rem]'/></a>
   </div>
 
@@ -178,13 +178,11 @@ function Testimonials(){
      alt="img" />
   </div>
 </div>
-<span className='md:text-xl md:ml-[45rem] text-7xl ml-[32rem] animate-pulse text-rose-400 font-descriptions'>Swipe left <FaLongArrowAltLeft className='md:ml-[45rem] w-20 ml-[37rem]'/></span>
+<span className='md:text-xl md:ml-[45rem] text-7xl ml-[32rem] animate-pulse text-korean-description font-descriptions'>Swipe left <FaLongArrowAltLeft className='md:ml-[45rem] w-20 ml-[37rem]'/></span>
 <div className='md:invisible divider pt-40 ml-32 mr-32'></div>
  </div>
     )
 }
-
-
 export default Testimonials;
 
 
