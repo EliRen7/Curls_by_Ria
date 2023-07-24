3f1486a27ac456382c88c045c7145da1cbdcc53c
Elizabeth Renderos <84453538+EliRen7@users.noreply.github.com>
2023-07-24 22:54:45 +0900

    Update Treatments.js

--- components/Treatments.js
+++ components/Treatments.js
@@ -7,8 +7,6 @@ import Prices1 from '../public/assets/prices1.jpg'
 import Prices2 from '../public/assets/prices2.jpg'
 import Prep1 from '../public/assets/prep1.jpg'
 import Prep2 from '../public/assets/prep2.jpg'
-import Deposit1 from '../public/assets/deposit1.jpg'
-import Deposit2 from '../public/assets/deposit2.jpg'
 
 import { useEffect } from 'react';
 
@@ -77,14 +75,14 @@ function Treatments(){
   <div className="grid md:grid-cols-2 md:ml-20 mt-48 md:mt-0 md:w-full md:h-90 card bg-white place-items-center">
     <section className="pt-5">
     <Image
-      src={Deposit1}
+      src={Prep1}
       className='w-[90rem] md:w-4/5 md:h-auto'
       alt='img'
       />
       </section>
       <section className="pt-20 md:pt-5">
       <Image
-      src={Deposit2}
+      src={Prep2}
       className='w-[90rem] md:w-4/5 md:h-auto'
       alt='img'
       />
