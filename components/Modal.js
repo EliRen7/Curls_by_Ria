5f4ea471f03dbbddb776d0eb9f320cbac224b42a
Elizabeth Renderos <84453538+EliRen7@users.noreply.github.com>
2023-07-27 09:27:15 +0900

    Update Modal.js

--- components/Modal.js
+++ components/Modal.js
@@ -21,9 +21,9 @@ function handleChange(event) {
         Book Now</label>
       <form>
       <input type="checkbox" id="my-modal-4" className="modal-toggle" />
-      <label htmlFor="my-modal-4" className="modal cursor-pointer">
+      <label htmlFor="my-modal-4" className="modal cursor-pointer font-header">
         <label htmlFor="" className="modal-box relative max-w-5xl pt-52 md:pt-5 md:pl-10 pl-20 max-h-full md:max-h-screen md:max-w-lg bg-white ">
-          <h3 className="text-6xl md:text-lg font-bold text-center">Welcome!</h3>
+          <h3 className="text-7xl md:text-2xl font-bold text-center">Welcome!</h3>
           <h4 className=" pt-5 m-5 text-5xl md:text-base">Step 1: Click to request an appointment date
           <br></br>1단계: 예약을 요청하려면 아래 버튼을 클릭하세요
           </h4>
