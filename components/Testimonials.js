514bf22d444206fa5c7e8609b547e6ef04b4942e
Elizabeth Renderos <84453538+EliRen7@users.noreply.github.com>
2024-03-19 05:42:36 +0900

    Update Testimonials.js

--- components/Testimonials.js
+++ components/Testimonials.js
@@ -109,13 +109,9 @@ function Testimonials(){
 </div>
 <span className='md:text-xl md:ml-[45rem] text-7xl ml-[32rem] animate-pulse text-korean-description font-header'>Swipe left <FaLongArrowAltLeft className='md:ml-[45rem] w-20 ml-[37rem]'/></span>
  </div>
-
-
    )
 }
-
 export default Testimonials;
-
 
 
 
