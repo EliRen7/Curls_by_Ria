8e3479009e43297c8eb83e3a434d970c68f7a5fb
Elizabeth Renderos <84453538+EliRen7@users.noreply.github.com>
2024-03-19 12:51:20 +0900

    Update Contact.js

--- components/Contact.js
+++ components/Contact.js
@@ -7,7 +7,7 @@ function Contact () {
   <div className="py-8 lg:py-16 px-4 mx-auto min-h-screen-xl max-w-screen-lg md:max-w-screen-md rounded-3xl shadow-xl bg-zinc-100 ">
       <h2 className="mb-4 text-8xl md:text-5xl tracking-tight text-center text-korean-description font-header">Contact Me!</h2>
       <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 text-5xl md:text-xl sm:text-xl">Got a technical issue? Want to send feedback about a service? Need more details about a treatment option? Let me know.</p>
-      <form action="https://formcarry.com/s/5pQxq6cPy5" method="POST" accept-charset="UTF-8" className="space-y-8">
+      <form action="https://formcarry.com/s/NHVjBk1PS3S" method="POST" accept-charset="UTF-8" className="space-y-8">
           <div>
               <label for="email" className="block mb-2 text-4xl md:text-sm font-medium text-gray-300 dark:text-gray-400 ">Your email</label>
               <input name="email" type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-4xl md:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:bg-white dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@curlsbyria.com" required />
