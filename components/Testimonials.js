32977bd8a37298bade899ab291c43dda029c99bc
Elizabeth Renderos <84453538+EliRen7@users.noreply.github.com>
2024-03-19 13:01:38 +0900

    Update Testimonials.js

--- components/Testimonials.js
+++ components/Testimonials.js
@@ -23,40 +23,7 @@ function Testimonials(){
       <a href='https://www.instagram.com/curlsbyria/?igshid=MzRlODBiNWFlZA%3D%3D' target="_blank"> <AiFillInstagram className='w-32 h-32 ml-[38rem] md:w-10 md:h-10 md:ml-[46rem]'/></a>
   </div>
 
-<div className='grid md:grid-cols-3 pb-20 pl-20 pr-20 gap-10'>
-  <div className="card w-full  bg-gradient-to-r from-orange-100 to-rose-100 text-gray-500" data-aos="flip-left">
-  <div className="card-body ">
-    <Image 
-        src={review4}
-        className='h-full'
-        alt='img' />
-    <div className="card-actions justify-end">
-    </div>
-  </div>
-</div>
-<div className="card w-full bg-gradient-to-r from-orange-100 to-rose-100 text-gray-500" data-aos="flip-right">
-  <div className="card-body">
-    <Image
-        src={review3} 
-        className='h-full' 
-        alt='img'/>
-    <div className="card-actions justify-end">
-    </div>
-  </div>
-</div>
-<div className="card w-full  bg-gradient-to-r from-orange-100 to-rose-100 text-gray-500" data-aos="flip-left">
-  <div className="card-body">
-    <Image 
-        src={review5}
-        className='h-full'
-        alt='img' />
-    <div className="card-actions justify-end">
-    </div>
-  </div>
-</div>
-</div>
-
-  <div className="carousel rounded-box ml-20 mr-20 md:w-fit md:h-80 lg:w-fit ">
+ <div className="carousel rounded-box ml-20 mr-20 md:w-fit md:h-80 lg:w-fit ">
   <div className="carousel-item">
     <Image
     src={hairimgs1} 
@@ -109,6 +76,93 @@ function Testimonials(){
   </div> 
 </div>
 <span className='md:text-xl md:ml-[45rem] text-7xl ml-[32rem] animate-pulse text-korean-description font-header'>Swipe left <FaLongArrowAltLeft className='md:ml-[45rem] w-20 ml-[37rem]'/></span>
+
+<div className='grid md:grid-cols-3 pb-20 pl-20 pr-20 gap-10 mt-10'>
+  <div className="card w-full  bg-gradient-to-r from-orange-100 to-rose-100 text-gray-500" data-aos="flip-left">
+  <div className="card-body ">
+    <Image 
+        src={review4}
+        className='h-full'
+        alt='img' />
+    <div className="card-actions justify-end">
+    </div>
+  </div>
+</div>
+<div className="card w-full bg-gradient-to-r from-orange-100 to-rose-100 text-gray-500" data-aos="flip-right">
+  <div className="card-body">
+    <Image
+        src={review3} 
+        className='h-full' 
+        alt='img'/>
+    <div className="card-actions justify-end">
+    </div>
+  </div>
+</div>
+<div className="card w-full  bg-gradient-to-r from-orange-100 to-rose-100 text-gray-500" data-aos="flip-left">
+  <div className="card-body">
+    <Image 
+        src={review5}
+        className='h-full'
+        alt='img' />
+    <div className="card-actions justify-end">
+    </div>
+  </div>
+</div>
+</div>
+
+//   <div className="carousel rounded-box ml-20 mr-20 md:w-fit md:h-80 lg:w-fit ">
+//   <div className="carousel-item">
+//     <Image
+//     src={hairimgs1} 
+//     className='w-[75rem] md:w-fit md:h-80 '
+//     alt="img" />
+//   </div> 
+//   <div className="carousel-item">
+//     <Image
+//     src={hairimgs2} 
+//     className='w-[75rem] md:w-fit md:h-80 '
+//     alt="img" />
+//   </div> 
+//   <div className="carousel-item">
+//     <Image
+//     src={hairimgs3}
+//     className='w-[75rem] md:w-fit md:h-80 '
+//     alt="img" />
+//   </div> 
+//   <div className="carousel-item">
+//     <Image
+//     src={hairimgs4}
+//     className='w-[75rem] md:w-fit md:h-80 '
+//     alt="img" />
+//   </div> 
+//   <div className="carousel-item">
+//     <Image
+//     src={hairimgs5}
+//     className='w-[75rem] md:w-fit md:h-80 '
+//      alt="img" 
+//      id='first' />
+//   </div> 
+//   <div className="carousel-item">
+//     <Image
+//     src={hairimgs6} 
+//     className='w-[75rem] md:w-fit md:h-80 '
+//     id='img' 
+//     alt="img" />
+//   </div> 
+//   <div className="carousel-item">
+//     <Image
+//     src={hairimgs7}
+//     className='w-[75rem] md:w-fit md:h-80 '
+//     alt="img" />
+//   </div> 
+//   <div className="carousel-item">
+//     <Image
+//     src={hairimgs8} 
+//     className='w-[75rem] md:w-fit md:h-80 ' 
+//     alt="img" />
+//   </div> 
+// </div>
+// <span className='md:text-xl md:ml-[45rem] text-7xl ml-[32rem] animate-pulse text-korean-description font-header'>Swipe left <FaLongArrowAltLeft className='md:ml-[45rem] w-20 ml-[37rem]'/></span>
  </div>
     )
 }
