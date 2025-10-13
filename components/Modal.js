8c624d5263e6db99ddcd966c6987b1cfff4323fa
Elizabeth Renderos <84453538+EliRen7@users.noreply.github.com>
2025-10-13 15:49:42 -0500

    Update Modal.js

--- components/Modal.js
+++ components/Modal.js
@@ -71,11 +71,11 @@ function handleChange(event) {
                 <option value='125'>Complete Curl Care $125 </option>
                 <option value='75'>Curl Cultivation $75 </option>
               </select>
-                 <h4 className="pt-5 m-5 text-5xl md:text-base"> Due to PayPal's transaction fees as well as bank remission fees
+          <Payments amount={price}/>
+                <h4 className="pt-5 m-5 text-5xl md:text-base"> Due to PayPal's transaction fees as well as bank remission fees
                  (altogether a total of $45 USD), those who use PayPal to send the deposit will need to send $125 for a Complete 
                  Curl Care Service or $75 for a Curl Cultivation Service. Thank you for your understanding! 
              </h4>
-          <Payments amount={price}/>
         </label>
       </label>
       </form>
